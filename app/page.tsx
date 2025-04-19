12c80572e7c3083bcb2a533ab8d833b85fece4a6
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2025-04-19 23:49:05 +0200

    🧩 Prepared some types for weight, fluid, etc. measurements in the metric and imperial systems

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,12 @@
-import { Ingredient } from "@garlic/types";
+import { cheesecake, vegetables } from "./ingredients";
 import { Ingredients, NutritionFacts } from "@garlic/ui";
 
 export default function Page() {
-  const className = "bg-blue-500 text-white p-4 rounded-lg";
-
-  const ingredients: Ingredient[] = [
-    {'name': 'onion', measure: {quantity: 1, unit: "unit"} },
-    {'name': 'garlic',  measure: {quantity: 3, unit: "head"} },
-  ];
-
   return (
     <div className="min-h-screen p-8 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col row-start-2 sm:items-start">
-        <Ingredients ingredients={ingredients} className={className} />
-        <NutritionFacts ingredients={ingredients} className={className} />
+        <Ingredients ingredients={vegetables} className="bg-blue-500 text-white p-4 rounded-lg" />
+        <NutritionFacts ingredients={cheesecake} className="bg-blue-500 text-white p-4 rounded-lg" />
       </main>
     </div>
   );
--- packages/types/src/index.d.ts
+++ packages/types/src/index.d.ts
@@ -1,64 +1,110 @@
-export type CaloriesUnit = "kcal" | "kJ";
+declare const metricFluidUnits: readonly ["l", "ml"];
+declare const imperialFluidUnits: readonly ["fl oz", "tsp", "tbsp", "cup"];
 
-export type UnitaryWeightUnit = "unit" | "head" | "clove" | "piece" | "slice" | "leaf" | "pinch";
-
-export type MetricFluidUnit = "l" | "ml";
-export type ImperialFluidUnit = "fl oz" | "tsp" | "tbsp" | "cup";
-
-export type MetricWeightUnit = "kg" | "g";
-export type ImperialWeightUnit = "lb" | "oz" | "cup";
+export type MetricFluidUnit = typeof metricFluidUnits[number];
+export type ImperialFluidUnit = typeof imperialFluidUnits[number];
 
 export type FluidUnit = MetricFluidUnit | ImperialFluidUnit;
+
+
+declare const metricWeightUnits: readonly ["kg", "g"];
+declare const imperialWeightUnits: readonly ["lb", "oz", "cup"];
+declare const unitaryWeightUnits: readonly [
+  "unit",
+  "head",
+  "leaf",
+  "clove",
+  "piece",
+  "slice",
+  "ounce",
+  "pinch"
+];
+
+export type MetricWeightUnit = typeof metricWeightUnits[number];
+export type ImperialWeightUnit = typeof imperialWeightUnits[number];
+export type UnitaryWeightUnit = typeof unitaryWeightUnits[number];
+
 export type WeightUnit = MetricWeightUnit | ImperialWeightUnit | UnitaryWeightUnit;
 
+
+export type MetricTemperatureUnit = "C";
+export type ImperialTemperatureUnit = "F";
+export type TemperatureUnit = MetricTemperatureUnit | ImperialTemperatureUnit;
+
+
+declare const caloriesUnits: readonly ["kcal", "kJ"];
+export type CaloriesUnit = typeof caloriesUnits[number];
+
+export type UnitSystem = "metric" | "imperial";
 export type MeasureUnit = WeightUnit | FluidUnit;
 
-export type Measure = {
-    unit: MeasureUnit,
-    quantity: number,
+export type MetricUnit =
+| MetricTemperatureUnit
+| MetricFluidUnit
+| MetricWeightUnit;
+
+export type ImperialUnit =
+| ImperialTemperatureUnit 
+| ImperialFluidUnit 
+| ImperialWeightUnit;
+
+export type Measure<T extends MeasureUnit | WeightUnit | FluidUnit> = {
+  unit: T,
+  quantity: number,
 };
 
 /**
  * Represents an ingredient.
  */
 export type Ingredient = {
-    name: string,
-    measure: Measure,
-    serving?: Measure,
-    nutritionFacts?: NutritionFacts,
+  name: string,
+  measure: Measure<MeasureUnit>,
+  serving?: Measure<MeasureUnit>,
+  nutritionFacts?: NutritionFacts,
 };
 
 export type CaloriesInfo = {
-    unit?: CaloriesUnit,
-    total: number,
+  unit?: CaloriesUnit,
+  total: number,
 };
 
 export type FatInfo = {
-    unit?: WeightUnit,
-    total: number,
-    saturated?: number,
+  unit?: WeightUnit,
+  total: number,
+  saturated?: number,
 };
 
 export type CarbsInfo = {
-    unit?: WeightUnit,
-    total: number,
-    sugars?: number,
-    fiber?: number,
+  unit?: WeightUnit,
+  total: number,
+  sugars?: number,
+  fiber?: number,
 };
 
 export type ProteinInfo = {
-    unit?: WeightUnit,
-    total: number,
+  unit?: WeightUnit,
+  total: number,
 };
 
 /**
  * Represents the nutrition facts of an ingredient.
  */
+// export type NutritionFacts = {
+//   measure: Measure<WeightUnit>,
+//   calories: CaloriesInfo,
+//   fat: FatInfo,
+//   carbs: CarbsInfo,
+//   protein: ProteinInfo,
+//   salt: number,
+// };
+
 export type NutritionFacts = {
-    unit: WeightUnit,
-    calories: CaloriesInfo,
-    fat: FatInfo,
-    carbs: CarbsInfo,
-    protein: ProteinInfo,
-    salt: number,
+  measure: Measure<WeightUnit>,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+  salt: number,
 };
+
+type NutritionFactsKeys = keyof Omit<NutritionFacts, "measure">;
