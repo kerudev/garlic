bd03f4e8ff083b1b0e4074bc5cf4aa5c45a7c074
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2026-09-19 23:54:10 +0200

    🔧 fix(components)[NutritionFacts]: calculate total correctly

--- packages/ui/src/components/NutritionFacts/NutritionFacts.tsx
+++ packages/ui/src/components/NutritionFacts/NutritionFacts.tsx
@@ -32,9 +32,14 @@ const getNutritionInfo = (ingredients: Ingredient[], servings?: Serving): [Nutri
     const facts = ingredient.nutritionFacts;
     if (!facts) return;
 
+    total.measure.quantity += ingredient.measure.quantity;
+
+    const ratio = ingredient.measure.quantity / facts.measure.quantity;
+
     rows.forEach((row) => {
-      total[row].quantity += facts[row].quantity ?? 0;
-      total.measure.quantity += facts.measure.quantity;
+      if (facts[row].quantity === 0) return;
+
+      total[row].quantity += facts[row].quantity * ratio;
     });
   });
 
@@ -51,22 +56,38 @@ const getNutritionInfo = (ingredients: Ingredient[], servings?: Serving): [Nutri
     ? Measure.fromObject({ quantity: total.measure.quantity / servings, unit: total.measure.unit })
     : servings;
 
+  // Round some values for convenience
   total.calories.quantity = Math.round(total.calories.quantity);
+  perServing.measure.quantity = Math.round(perServing.measure.quantity);
   perServing.calories.quantity = Math.round(perServing.calories.quantity);
 
   return [total, perServing];
 };
 
+const getServings = (total: number, serving?: Serving): [number, boolean] => {
+  if (typeof serving === "undefined") return [0, false];
+  if (typeof serving === "number") return [serving, false];
+
+  const s = total / serving?.quantity;
+  if (Number.isInteger(s)) return [s, false];
+
+  return [Math.round(s), true];
+};
+
 export default function NutritionFacts({ ingredients, className, options }: NutritionFactsProps) {
   const [total, perServing] = getNutritionInfo(ingredients, options?.servings);
+
+  const [nServings, isRounded] = getServings(total.measure.quantity, options?.servings);
+  const servingsText = nServings && isRounded ? `${nServings} servings` : `${nServings} aprox. servings`;
 
   return (
     <table className={className}>
       <thead className="h-[42] w-[120]">
-        <tr className="[&>*]:px-[8px] [&>*]:py-[8px]">
+        {/* Iterate over each row */}
+        <tr className="[&>*]:px-[8px] [&>*]:py-[8px] align-top">
           <th></th>
-          <th className="text-right !pr-4 w-50">Total</th>
-          {perServing && <th className="text-right !pl-0.5 w-50">Serving</th>}
+          <th className="rounded-tl-lg text-right !pr-4 w-50">Total<br />({servingsText})</th>
+          {perServing && <th className="text-right !pl-0.5 w-50">Per serving</th>}
         </tr>
         <tr className="[&>*]:px-[8px] [&>*]:py-[8px]">
           <th className="text-left">Values</th>
@@ -75,6 +96,7 @@ export default function NutritionFacts({ ingredients, className, options }: Nutr
         </tr>
       </thead>
       <tbody>
+        {/* Iterate over each row */}
         {rows.map((row, idx) => (
           <tr key={`fact-${row}-${idx}`} className="[&>*]:px-[8px] [&>*]:py-[8px]">
             <td className="text-left">{row}</td>
